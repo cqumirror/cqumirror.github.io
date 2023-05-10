f37efbfb167610bcc15c1a23ad7883158e0e3eba
weearc <qby19981121@gmail.com>
2023-05-11 02:18:28 +0800

    init site

---
+++ _nuxt/cbb9d7d.js
@@ -0,0 +1,1 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[3],{600:function(n,t,c){"use strict";c.r(t);var e={name:"IconClipboardCheck",data:function(){return{}}},o=c(23),component=Object(o.a)(e,(function(){return(0,this._self._c)("div",{attrs:{id:"clipboard-check"}})}),[],!1,null,null,null);t.default=component.exports}}]);
---
+++ _nuxt/cf342af.js
@@ -0,0 +1,1 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[2],{592:function(t,n,o){"use strict";o.r(n);var c="https://mirrors.cqu.edu.cn/";n.default={getMirror:function(t){return c+"/static/tunasync.json"},getIsoList:function(t){return c+"/static/isoinfo.json"},getNotices:function(t){return c+"/static/notice.json"}}}}]);
